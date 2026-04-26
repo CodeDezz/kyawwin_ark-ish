e2c3a509a94000ff78e91548bf141030a475c87f
JSTBetter-1 <winmyatkyaw13@gmail.com>
2026-04-26 00:01:00 -0700

    KyawWin_Finial
    added a menu that doesn't work, and code for interaction.

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -1,8 +1,11 @@
 extends CollisionObject3D
 class_name Interactable
 
-@export var prompt_message = "interact"
-@export var prompt_input = "interact"
+@export var prompt_message = "Interact"
+@export var hunger_restore_amount: float = 20.0
 
-func interact(body):
-	print(vody.name," interacted with ", name)
+func _on_body_entered(body: Interactable) -> void:
+	# Check if the object entering is the player
+	if body.has_method("add_hunger"):
+		body.add_hunger(hunger_restore_amount)
+		queue_free() # Removes the item from the game
